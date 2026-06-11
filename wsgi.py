b73b4b40a1772fd8d895df3682e8cb1a95863271
Raman-45 <ramanloniya857@gmail.com>
2026-06-11 14:23:26 +0530

    Fix wsgi.py path for PythonAnywhere

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,11 @@
-# PythonAnywhere WSGI configuration file
-# This file contains the WSGI configuration required to serve up your
-# web application at http://yourusername.pythonanywhere.com/
-
 import sys
 import os
 
-# Add your project directory to the sys.path
-project_home = '/home/yourusername/RMMV'  # <-- CHANGE 'yourusername' to your PythonAnywhere username
+project_home = '/home/Raman45/RMMV'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-# Set the working directory
 os.chdir(project_home)
 
-# Import your Flask app
 from app import create_app
 application = create_app()
